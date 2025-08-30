edc5cb75f4c6a352839aecd77d6a955e5c264619
Gbkng <guillaume.brooking@gmail.com>
2025-08-30 18:27:30 +0200

    feat: transform function into an app

--- recdl.sh
+++ recdl.sh
@@ -5,7 +5,7 @@ set -u
 which "fzf" >/dev/null 2>&1 ||
   {
     echo "'fzf' could be found in the current environment. This dependency is required. Abort." >&2;
-    return 1
+    exit 1
   }
 
 # note: also echo ".." to allow going backward in recdl
@@ -23,61 +23,54 @@ elif which "find" >/dev/null 2>&1; then
   }
 else
   echo "Neither 'fd' nor 'find' could be found in the current environment. These dependencies are required. Abort." >&2
-  return 1
+  exit 1
 fi
 
-recdl() {
-  base="$(pwd)"
-  newdir_relative="./"
-  while true; do
-    # a buffer is required, as return value of fzf is irrelevant in case of
-    # error or interrupt
-    buff="$(
-      fd_cmd "$(realpath --relative-to="$base" "$newdir_relative")" | fzf \
-        --layout=reverse \
-        --smart-case \
-        --algo=v2 \
-        --style=full \
-        --height=40%
-    )"
-    exit_status=$?
+base="$(pwd)"
+newdir_relative="./"
+while true; do
+  # a buffer is required, as exit value of fzf is irrelevant in case of
+  # error or interrupt
+  buff="$(
+    fd_cmd "$(realpath --relative-to="$base" "$newdir_relative")" | fzf \
+      --layout=reverse \
+      --smart-case \
+      --algo=v2 \
+      --style=full \
+      --height=40%
+  )"
 
-    # 130 is an expected SIGINT interrupt of fzf (see fzf manpage)
-    [ $exit_status -eq 130 ] && {
-      cd "$newdir_relative" || {
-        echo "Error: unexpected error while trying to 'cd' to '$newdir_relative'" >&2
-        return 1
-      }
-      # output pwd to stdout makes the function composable
-      echo "$newdir_relative"
-      return 0
-    }
+  exit_status=$?
 
-    # No match (see fzf manpage)
-    [ $exit_status -eq 1 ] && {
-      echo "Warning: impossible to 'cd' to given directory as it is not part of possible choices." >&2
-      continue
-    }
-    [ $exit_status -eq 2 ] && {
-      echo "Error: unexpected error of fzf" >&2
-      return 1
-    }
-    [ $exit_status -eq 0 ] || {
-      echo "Error: unexpected error status: '$exit_status'. Abort." >&2
-      return 1
-    }
+  # 130 is an expected SIGINT interrupt of fzf (see fzf manpage)
+  [ $exit_status -eq 130 ] && {
+    # output pwd to stdout makes the function composable with cd
+    echo "$newdir_relative"
+    exit 0
+  }
 
-    newdir_relative=$buff
+  # No match (see fzf manpage)
+  [ $exit_status -eq 1 ] && {
+    echo "Warning: impossible to 'cd' to given directory as it is not part of possible choices." >&2
+    continue
+  }
+  [ $exit_status -eq 2 ] && {
+    echo "Error: unexpected error of fzf" >&2
+    exit 1
+  }
+  [ $exit_status -eq 0 ] || {
+    echo "Error: unexpected error status: '$exit_status'. Abort." >&2
+    exit 1
+  }
 
-    [ -d "$newdir_relative" ] || {
-      echo "Error: unexpectedly, '$newdir_relative' is not a directory. Abort." >&2
-      return 1
-    }
+  newdir_relative=$buff
 
-    # clear line and print current
-    # see tput manpage
-    printf "%s> %s" "$(tput el)" "$newdir_relative" >&2
-  done
-}
+  [ -d "$newdir_relative" ] || {
+    echo "Error: unexpectedly, '$newdir_relative' is not a directory. Abort." >&2
+    exit 1
+  }
 
-alias hrecdl="cd \$HOME; recdl"
+  # clear line and print current
+  # see tput manpage
+  printf "%s> %s" "$(tput el)" "$newdir_relative" >&2
+done
